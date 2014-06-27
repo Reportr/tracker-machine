1f1fcf28b1537e878d3776b96bf7331a750b1eb5
Samy Pessé <samypesse@gmail.com>
2014-06-27 19:38:45 +0200

    Base version

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-tracker-machine
+Machine State Tracker
 ===============
 
-Monitor machine state (memory, load average)
+Monitor machine state (memory, load average).
+
+## How to use it?
+
+```
+$ npm install tracker-machine -g
+```
+
+To start it:
+
+```
+$ tracker-machine --host=http://localhost:5000
+```
+
+## Options
+
+```
+-i, --interval             Post interval (in seconds), defaults is 5 minutes
+-hs, --host [host]         Reportr hostname
+-u, --username [username]  Authentication username
+-p, --password [password]  Authentication password
+-e, --event [event]        Event name, default is "machine.state"
+```
+
---
+++ bin/tracker.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+
+var os = require('os');
+var _ = require('lodash');
+var program = require('commander');
+var pkg = require('../package.json');
+var Reportr = require('reportr-api');
+
+program
+.version(pkg.version)
+.option('-i, --interval', 'Post interval (in seconds), defaults is 5 minutes', parseInt)
+.option('-hs, --host [host]', 'Reportr hostname')
+.option('-u, --username [username]', 'Authentication username')
+.option('-p, --password [password]', 'Authentication password')
+.option('-e, --event [event]', 'Event name, default is "machine.state"')
+.parse(process.argv);
+
+program.interval = program.interval || 5*60;
+program.event = program.event || "machine.state";
+
+if (!program.host) {
+    console.log("Need 'host'");
+    return process.exit(-1);
+}
+
+console.log("Configure with host:", program.host);
+var client = new Reportr({
+    host: program.host,
+    auth: (program.username? { 'username': program.username, 'password': program.password } : null)
+});
+
+var update = function() {
+    var properties = {
+        'loadavg': _.first(os.loadavg()),
+        'totalmem': os.totalmem(),
+        'freemem': os.freemem()
+    };
+
+    client.postEvent(program.event, properties)
+    .then(function() {
+        console.log("posted");
+    }, function(e) {
+        console.log("failed:", e.message || e);
+    });
+};
+
+setInterval(update, program.interval*1000);
+update();
